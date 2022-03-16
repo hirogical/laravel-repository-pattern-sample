e418d54d1207e1ceb5288b7142ccb5e262112d83
hirogical <68487631+hirogical@users.noreply.github.com>
2022-03-16 22:00:41 +0900

    [update]

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -16,28 +16,9 @@ class UserRepository implements UserRepositoryInterface
 
     public function index()
     {
-        // $users = User::with('posts')
-        //     ->whereHas('posts', function ($q) {
-        //         $q->where('created_at', '>=', today());
-        //     })->find(1);
-
-        $users = User::whereHas('posts', function ($q) {
+        $users = User::withWhereHas('posts', function ($q) {
             $q->where('created_at', '>=', today());
-        })->find(1);
-
-        // dd($users->first()->posts);
-
-        // foreach ($users as $user) {
-        //     dd($user->posts);
-        // }
-
-        // $users = $this->user::withWhereHas('posts', function ($q) {
-        //     $q->where('created_at', '>=', today());
-        // })->get();
-
-        // $users = User::withWhereHas('posts', function ($q) {
-        //     $q->where('created_at', '>=', today());
-        // })->first();
+        })->first();
 
         return $users;
     }
